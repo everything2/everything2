b3b21692ac094b7a03d4e7d47ae8cf116b844eb9
Brian Orlick <Brian.Orlick@gmail.com>
2010-02-12 23:16:54 +0000

    Check to see if semaphores are running out, and release them if so. This can happen when apache gets killed repeatedly by this script, and doesn't get a chance to release its semaphores.  To combat this problem, I also put a longer delay between requesting the shutdown of apache and the 'kill -9'ing of it.

--- bin/checkApache.pl
+++ bin/checkApache.pl
@@ -2,8 +2,30 @@
 
 use strict;
 
+my $max_semaphores = 35;
+my @semaphores = `ipcs -s|grep 'www-data\\|apache'`;
+
+if (scalar @semaphores > $max_semaphores) {
+
+	`/etc/init.d/apache2 stop`;
+	sleep 5;
+	`killall -9 apache2`;
+	sleep 2;
+	
+	# Semaphores may have been released once apache shut down
+	#  so we refresh our list so we don't kill stale ids
+	@semaphores = `ipcs -s|grep 'www-data\\|apache'`;
+	for my $sem_line (@semaphores) {
+		my $semid = (split /\s+/, $sem_line)[1];
+		`ipcrm -s $semid`;
+	}
+	`/etc/init.d/apache2 start`;
+	sleep 2;
+
+}
+
 open MEMINFO, "/proc/meminfo";
-    
+
 my %info = ();
 while (<MEMINFO>) {
 	my ($name, $value, $unit) = split /\s+/;
@@ -16,11 +38,9 @@ my $swap = $info{'SwapTotal:'} - $info{'SwapFree:'};
 
 if ($swap > 150000) { #150 megs into swap
 	`/etc/init.d/apache2 stop`;
-	sleep 2;
+	sleep 5;
 	`killall -9 apache2`;
 	sleep 2;
 	`/etc/init.d/apache2 start`;
 }
-
-
 
