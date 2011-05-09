41b35210f329635fa9d38868a69a41d79314c1b9
Brian Orlick <Brian.Orlick@gmail.com>
2011-05-09 18:46:56 +0000

    Fix up cache store script to delete only remove old entries and not drop the tables outright.

--- bin/cache_store.sql
+++ bin/cache_store.sql
@@ -1,16 +1,14 @@
-drop table cache_store;
-drop table stats;
-
 --
 -- Table structure for table 'cache_store'
 --
 
-CREATE TABLE cache_store (
+CREATE TABLE IF NOT EXIST cache_store (
   cache_store_id int(11) NOT NULL auto_increment,
   page text,
   expired int(11) NOT NULL default '0',
   version int(11) NOT NULL default '0',
   tstamp timestamp,
+  INDEX timestamp (timestamp),
   PRIMARY KEY  (cache_store_id)
 ) TYPE=MyISAM;
 
@@ -18,10 +16,14 @@ CREATE TABLE cache_store (
 -- Table structure for table 'stats'
 --
 
-CREATE TABLE stats (
+CREATE TABLE IF NOT EXIST stats (
   stats_id int(11) NOT NULL auto_increment,
   hits int(11) default '0',
   miss int(11) default '0',
   PRIMARY KEY  (stats_id)
 ) TYPE=MyISAM;
+
+DELETE FROM cache_store JOIN stats
+  ON cache_store_id = stats_id
+  WHERE cache_store.timestamp < DATESUB(NOW(), INTERVAL 60 MINUTE)
 
