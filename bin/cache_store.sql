4232ff2073b0ca2ce4a77a1cdc8664d1c280a988
Brian Orlick <Brian.Orlick@gmail.com>
2011-05-09 18:51:44 +0000

    Fix errors in last cache_store script commit.

--- bin/cache_store.sql
+++ bin/cache_store.sql
@@ -2,13 +2,13 @@
 -- Table structure for table 'cache_store'
 --
 
-CREATE TABLE IF NOT EXIST cache_store (
+CREATE TABLE IF NOT EXISTS cache_store (
   cache_store_id int(11) NOT NULL auto_increment,
   page text,
   expired int(11) NOT NULL default '0',
   version int(11) NOT NULL default '0',
   tstamp timestamp,
-  INDEX timestamp (timestamp),
+  INDEX tstamp (tstamp),
   PRIMARY KEY  (cache_store_id)
 ) TYPE=MyISAM;
 
@@ -16,14 +16,16 @@ CREATE TABLE IF NOT EXIST cache_store (
 -- Table structure for table 'stats'
 --
 
-CREATE TABLE IF NOT EXIST stats (
+CREATE TABLE IF NOT EXISTS stats (
   stats_id int(11) NOT NULL auto_increment,
   hits int(11) default '0',
   miss int(11) default '0',
   PRIMARY KEY  (stats_id)
 ) TYPE=MyISAM;
 
-DELETE FROM cache_store JOIN stats
+DELETE
+  cache_store, stats
+  FROM cache_store JOIN stats
   ON cache_store_id = stats_id
-  WHERE cache_store.timestamp < DATESUB(NOW(), INTERVAL 60 MINUTE)
+  WHERE cache_store.tstamp < DATE_SUB(NOW(), INTERVAL 60 MINUTE)
 
