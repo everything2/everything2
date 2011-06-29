46b6d66757716b3e0326c879ac4c8c3da7bdc813
Brian Orlick <Brian.Orlick@gmail.com>
2011-06-29 22:13:33 +0000

    Make catbox clearing script use date comparison logic which is less likely to fail.

--- bin/cleanCbox.pl
+++ bin/cleanCbox.pl
@@ -3,12 +3,14 @@
 use Everything;
 initEverything 'everything';
 
+my $expireInSeconds = 500;
+
 my $messageSaveSQL = <<ENDSQL;
 INSERT INTO publicmessages
   (message_id, msgtext, tstamp, author_user)
   SELECT message_id, msgtext, tstamp, author_user
     FROM message
-    WHERE (NOW() - 500) > tstamp
+    WHERE TIMESTAMPADD(SECOND, -$expireInSeconds, NOW()) > tstamp
       AND for_user = 0
     ORDER BY tstamp ASC
   ON DUPLICATE KEY UPDATE
@@ -17,7 +19,7 @@ ENDSQL
 my $dbh = $DB->getDatabaseHandle();
 $dbh->do($messageSaveSQL);
 
-$DB->sqlDelete("message", "for_user=0 AND (NOW() - 500) > tstamp");
+$DB->sqlDelete("message", "for_user=0 AND TIMESTAMPADD(SECOND, -$expireInSeconds, NOW()) > tstamp");
 #clean up the chatterbox table
 
 
