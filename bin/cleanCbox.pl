4cf7440e211e4e3d8d026932c8314833a074544a
Brian Orlick <Brian.Orlick@gmail.com>
2011-06-29 02:06:09 +0000

    Start locally saving outside messages in a simplified public message table.

--- bin/cleanCbox.pl
+++ bin/cleanCbox.pl
@@ -3,7 +3,21 @@
 use Everything;
 initEverything 'everything';
 
-$DB->sqlDelete("message", "for_user=0 and now()-tstamp > 500");
+my $messageSaveSQL = <<ENDSQL;
+INSERT INTO publicmessages
+  (message_id, msgtext, tstamp, author_user)
+  SELECT message_id, msgtext, tstamp, author_user
+    FROM message
+    WHERE (NOW() - 500) > tstamp
+      AND for_user = 0
+    ORDER BY tstamp ASC
+  ON DUPLICATE KEY UPDATE
+    publicmessages.tstamp = message.tstamp
+ENDSQL
+my $dbh = $DB->getDatabaseHandle();
+$dbh->do($messageSaveSQL);
+
+$DB->sqlDelete("message", "for_user=0 AND (NOW() - 500) > tstamp");
 #clean up the chatterbox table
 
 
