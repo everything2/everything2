69cab349a8b4ac13453f600a58e16616c4f1cb2b
Brian Orlick <Brian.Orlick@gmail.com>
2011-11-19 06:24:38 +0000

    Generating static stylesheets using existing serve/view pages so they get additional massaging as necessary.  Generate autofixed and non-autofixed versions.

--- bin/generateStylesheets.pl
+++ bin/generateStylesheets.pl
@@ -5,20 +5,21 @@ use lib "/var/everything/ecore";
 use strict;
 use Everything;
 use Everything::HTML;
-use Everything::CacheStore;
-use POSIX qw(strftime);
-use Time::HiRes qw(gettimeofday tv_interval);
+require CGI;
 
 my $STYLESHEET_PATH = '/var/everything/www/stylesheet/';
 
 initEverything "everything", 0, { servers => ["127.0.0.1:11211"] };
 %Everything::HTML::HTMLVARS = %{ eval (getCode('set_htmlvars')) };
+$Everything::HTML::USER = getNode('Guest User', 'user');
+$query = new CGI();
 
 mkdir $STYLESHEET_PATH unless -e $STYLESHEET_PATH;
 
 my $forceUpdate = grep { /--force/ } @ARGV;
 my @title_list = grep { $_ !~ /^-/; } @ARGV;
 my $titleRestrict = '';
+
 
 if (@title_list) {
 	$titleRestrict = "title IN ("
@@ -29,25 +30,45 @@ if (@title_list) {
 }
 
 my @stylesheets = $DB->getNodeWhere($titleRestrict, 'stylesheet');
-
+my %displaytypes = (
+	'basesheet'		=>		'view',
+	'print'			=>		'view',
+);
 
 foreach my $stylesheetNode (@stylesheets) {
 
-	my $version = $DB->{cache}->getGlobalVersion($stylesheetNode);
-	my $escapeTitle = CGI::escape($$stylesheetNode{title});
-	$escapeTitle =~ s/%[0-9A-F]{2}|_//g;
-	my $stylesheetFilename = "${escapeTitle}_v$version.css";
-	my $stylesheetFilepath = $STYLESHEET_PATH . $stylesheetFilename;
-	my $stylesheetFilepathbase = $STYLESHEET_PATH . $escapeTitle . ".css";
-	next if -e $stylesheetFilepath and not $forceUpdate;
+	foreach my $autofix (qw/0 1/) {
 
-	my $fh = FileHandle->new($stylesheetFilepath, "w");
-	if (!$fh) {
-		print "Failed to create $stylesheetFilepath when generating stylesheet. : $@\n";
-	} else {
-		$fh->print($$stylesheetNode{doctext});
-		undef $fh;
-		unlink $stylesheetFilepathbase if -l $stylesheetFilepathbase;
-		symlink($stylesheetFilepath, $stylesheetFilepathbase);
+			my $autofixFilename = $autofix ? "_autofix" : "";
+			my $version = $DB->{cache}->getGlobalVersion($stylesheetNode);
+			my $escapeTitle = CGI::escape($$stylesheetNode{title});
+			$escapeTitle =~ s/%[0-9A-F]{2}|_//g;
+			my $stylesheetFilename = "${escapeTitle}_v${version}${autofixFilename}.css";
+			my $stylesheetFilepath = $STYLESHEET_PATH . $stylesheetFilename;
+			my $stylesheetFilepathbase = $STYLESHEET_PATH . $escapeTitle . "$autofixFilename.css";
+			next if -e $stylesheetFilepath and not $forceUpdate;
+
+			my $displaytype = 'serve';
+			$displaytype = $displaytypes{$escapeTitle} if exists $displaytypes{$escapeTitle};
+			
+			my $outputter = getPage($stylesheetNode, $displaytype);
+			$Everything::HTML::GNODE = $stylesheetNode;
+			my $out = parseCode($$outputter{page}, $stylesheetNode);
+			if ($autofix) {
+				$query->param('autofix', 1);
+			} else {
+				$query->delete('autofix');
+			}
+			my $fh = FileHandle->new($stylesheetFilepath, "w");
+			if (!$fh) {
+				print "Failed to create $stylesheetFilepath when generating stylesheet. : $@\n";
+			} else {
+				$fh->print($out);
+				undef $fh;
+				unlink $stylesheetFilepathbase if -l $stylesheetFilepathbase;
+				symlink($stylesheetFilepath, $stylesheetFilepathbase);
+			}
+
 	}
+
 }
