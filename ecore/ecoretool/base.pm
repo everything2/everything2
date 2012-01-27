e3a1ef20a1a77ca31cde5582f605b9bdd6fe0e43
Jay Bonci <jay@bonci.net>
2012-01-27 13:33:19 -0500

    Genericized ecoretool help and argument parsing, also correct bootstrapping for reduced nodepack directories

--- ecore/ecoretool/base.pm
+++ ecore/ecoretool/base.pm
@@ -3,6 +3,8 @@
 use strict;
 package ecoretool::base;
 
+use Getopt::Long;
+
 sub new
 {
 	my ($class) = @_;
@@ -11,6 +13,92 @@ sub new
 	return bless $this,$class;
 }
 
+sub _inputs
+{
+	return {};
+}
+
+sub _handle_inputs
+{
+	my ($this) = @_;
+
+	my $getoptargs;
+
+	my $inputs = $this->_inputs();
+
+	my $options;
+
+	foreach my $input_key(keys %$inputs)
+	{
+		my $firstarg = join("|",$input_key,@{$inputs->{$input_key}->{alias}});
+		if(exists($inputs->{$input_key}->{type}))
+		{
+			$firstarg.="=".$inputs->{$input_key}->{type};
+		}
+		push @$getoptargs,$firstarg,\$options->{$input_key};
+	}
+
+	GetOptions(@$getoptargs);
+
+	foreach my $input_key(keys %$inputs)
+	{
+		if(not defined($options->{$input_key}))
+		{
+			$options->{$input_key} = $inputs->{$input_key}->{default};
+		}
+	}
+
+	return $options;
+}
+
+sub help
+{
+	my ($this) = @_;
+	
+	my $inputs = $this->_inputs();
+	my $thismodule = ref $this;
+	$thismodule =~ s/.*:://g;
+
+	print "Help for ecoretool.pl $thismodule:\n";
+
+	my $maxlength = 0;
+	foreach my $input_key (keys %$inputs)
+	{
+		my $thislength = length($this->_format_help_alias($input_key));
+		if($maxlength < $thislength)
+		{
+			$maxlength = $thislength;
+		}
+	}
+
+	foreach my $input_key (keys %$inputs)
+	{
+		my $ha = $this->_format_help_alias($input_key);
+		print " $ha".(" " x ($maxlength - length($ha)))."\t".$inputs->{$input_key}->{help}."\n";
+	}
+	return;
+}
+sub _format_help_alias
+{
+	my ($this, $input_key) = @_;
+
+	my $inputs = $this->_inputs();
+	
+	my $str = "--$input_key";
+
+	foreach my $alias (@{$inputs->{$input_key}->{alias}})
+	{
+		if(length($alias) == 1)
+		{
+			$str.=",-$alias";
+		}else{
+			$str.=",--$alias";
+		}
+	}
+
+	return $str;
+
+}
 sub get_worker_object
 {
 	my ($this, $type) = @_;
---
+++ ecore/ecoretool/help.pm
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+use utf8;
+use lib qw(lib);
+
+use ecoretool::base;
+package ecoretool::help;
+use base qw(ecoretool::base);
+
+sub main
+{
+	my $topic = $ARGV[1];
+	
+	my $obj;
+	$topic =~ s/\W//g;
+	eval("require ecoretool::$topic; \$obj = ecoretool::$topic->new()");
+	
+	if(defined($obj))
+	{
+		$obj->help();
+	}else{
+		print "No help available for $topic";
+	}
+}
+
+sub shortdesc
+{
+	return "Show help for a particular module. ecoretool.pl help <module>";
+}
+
+1;
