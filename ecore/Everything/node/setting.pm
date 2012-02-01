f49bb5507a0e6cbef6f88e660d2d5194160da627
Jay Bonci <jay@bonci.net>
2012-01-31 22:01:03 -0500

    Ignore setting_id in setting

---
+++ ecore/Everything/node/setting.pm
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(lib);
+package Everything::node::setting;
+use base qw(Everything::node::node);
+
+sub node_id_equivs
+{
+	my ($this) = @_;
+	return ["setting_id",@{$this->SUPER::node_id_equivs}];
+}
+
+1;
