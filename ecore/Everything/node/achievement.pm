6c4257d9a07008a0e93a1cb6d9fe1c1f1383cecd
Jay Bonci <jay@bonci.net>
2012-01-31 22:51:30 -0500

    Clean up the exporting of the achievement type; collapse achievement id and clean the code field

---
+++ ecore/Everything/node/achievement.pm
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(lib);
+package Everything::node::achievement;
+use base qw(Everything::node::node);
+
+sub node_to_xml
+{
+	my ($this, $NODE, $dbh) = @_;
+
+	# Strip old windows line endings
+	$NODE->{code} = $this->_clean_code($NODE->{code});
+	return $this->SUPER::node_to_xml($NODE, $dbh);
+}
+
+sub node_id_equivs
+{
+	my ($this) = @_;
+	return ["achievement_id", @{$this->SUPER::node_id_equivs()}];
+}
+
+1;
