9c8727989e65ab0b9bd349abc24726ab4e0bf43f
Jay Bonci <jay@bonci.net>
2012-01-31 23:02:16 -0500

    Add in codearchive cleanup. This is a bit of a hack since it decends from multiple types, which ecore allows but perl does not

---
+++ ecore/Everything/node/codearchive.pm
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(lib);
+package Everything::node::codearchive;
+use base qw(Everything::node::htmlcode);
+
+sub node_to_xml
+{
+	my ($this, $NODE, $dbh) = @_;
+
+	# Strip old windows line endings
+	$NODE->{doctext} = $this->_clean_code($NODE->{doctext});
+	return $this->SUPER::node_to_xml($NODE, $dbh);
+}
+
+sub node_id_equivs
+{
+	my ($this) = @_;
+	return ["document_id", @{$this->SUPER::node_id_equivs()}];
+}
+
+1;
