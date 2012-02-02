33f0c493d594b8efe4d9ea05b194a587314e602c
Jay Bonci <jay@bonci.net>
2012-02-01 23:13:01 -0500

    Clean up nodeball export

--- ecore/Everything/node/nodeball.pm
+++ ecore/Everything/node/nodeball.pm
@@ -5,4 +5,10 @@ use lib qw(lib);
 package Everything::node::nodeball;
 use base qw(Everything::node::nodegroup);
 
+sub node_id_equivs
+{
+	my ($this) = @_;
+	return ["setting_id", @{$this->SUPER::node_id_equivs()}];
+}
+
 1;
