ebe1253bb93e92668caebda7baae267c71e9d9d2
Jay Bonci <jay@bonci.net>
2012-02-04 18:11:01 -0500

    Support for usergroups to clean out their contents

--- ecore/Everything/node/usergroup.pm
+++ ecore/Everything/node/usergroup.pm
@@ -5,4 +5,26 @@ use lib qw(lib);
 package Everything::node::usergroup;
 use base qw(Everything::node::nodegroup);
 
+# Stolen from document, though I had to:
+sub node_to_xml
+{
+	my ($this, $NODE, $dbh) = @_;
+
+	# Strip old windows line endings
+	$NODE->{doctext} = $this->_clean_code($NODE->{doctext});
+	return $this->SUPER::node_to_xml($NODE, $dbh);
+}
+
+sub xml_no_consider
+{
+	my ($this) = @_;
+	return ["edittime",@{$this->SUPER::xml_no_consider()}];
+}
+
+sub node_id_equivs
+{
+	my ($this) = @_;
+	return ["document_id", @{$this->SUPER::node_id_equivs()}];
+}
+
 1;
