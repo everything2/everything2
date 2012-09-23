bdb62373ae4d96d1e67ca8a24b718c71a742f0ed
Jay Bonci <jay@bonci.net>
2012-09-23 00:38:24 -0400

    Update the e2cron entries to use the API instead of just dumping a file in there

--- vagrant/cookbooks/e2cron/recipes/default.rb
+++ vagrant/cookbooks/e2cron/recipes/default.rb
@@ -7,11 +7,45 @@
 # You are free to use/modify these files under the same terms as the Everything Engine itself
 #
 
-template '/etc/cron.d/e2cron' do
-  owner "root"
-  group "root"
-  mode "0755"
-  action "create"
-  source 'e2cron.erb'
+file '/etc/cron.d/e2cron' do
+   action "delete"
 end
 
+cron 'updateNodelet.pl' do
+  user "root"
+  minute "0-59/5"
+  command "/var/everything/ecore/bin/updateNodelet.pl"
+end
+
+cron 'refreshRoom.pl' do
+  user "root"
+  minute "0-59/5"
+  command "/var/everything/ecore/bin/refreshRoom.pl"
+end
+
+cron 'cleanCbox.pl' do
+  user "root"
+  minute "50"
+  command "/var/everything/ecore/bin/cleanCbox.pl"
+end
+
+cron 'newstats.pl' do
+  user "root"
+  minute "10"
+  hour "6"
+  command "/var/everything/ecore/bin/newstats.pl"
+end
+
+cron 'expirerooms.pl' do
+  user "root"
+  minute "30"
+  hour "6"
+  command "/var/everything/ecore/bin/expirerooms.pl"
+end
+
+cron 'reaper.pl' do
+  user "root"
+  minute "50"
+  hour "6"
+  command "/var/everything/ecore/bin/reaper.pl"
+end
