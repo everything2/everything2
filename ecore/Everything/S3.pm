9d950ac8ed7a9c234b388aae7fcf5de6e525fa19
Jay Bonci <jay@bonci.net>
2012-09-26 00:22:45 -0400

    Add in automated database backup to S3

---
+++ ecore/Everything/S3.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Everything;
+use Net::Amazon::S3;
+package Everything::S3;
+
+sub new
+{
+	my ($class, $s3type) = @_;
+	
+	return if not defined $s3type;
+	my $this = {};
+	if(exists($Everything::CONF->{s3}->{$s3type}))
+	{
+		foreach my $value (qw/bucket access_key_id secret_access_key/)
+		{
+			if(exists($Everything::CONF->{s3}->{$s3type}->{$value}))
+			{
+				$this->{$value} = $Everything::CONF->{s3}->{$s3type}->{$value};
+			}else{
+				return undef;
+			}
+		}
+	}else{
+		return undef;
+	}
+
+	$this->{s3} = Net::Amazon::S3->new(
+	{   
+		aws_access_key_id     => $this->{access_key_id},
+		aws_secret_access_key => $this->{secret_access_key},
+		retry                 => 1,
+	});
+
+	return unless defined($this->{s3});
+	$this->{bucket} = $this->{s3}->bucket($this->{bucket});
+
+	return bless $this,$class;
+}
+
+sub upload_data
+{
+	my ($this, $name, $data, $properties) = @_;
+}
+
+sub upload_file
+{
+	my ($this, $name, $filename, $properties) = @_;
+	return $this->{bucket}->add_key_filename($name, $filename);
+}
+
+sub delete
+{
+	my ($this, $filename) = @_;
+}
+
+1;
---
+++ tools/database_backup_to_s3.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(/var/everything/ecore);
+use Everything;
+use Everything::S3;
+use Everything::MAIL;
+
+my $uploader = Everything::S3->new("backup");
+my $tmpdir = "/tmp/everything_backup_$$";
+`mkdir -p $tmpdir`;
+
+my $user = $Everything::CONF->{'everyuser'};
+my $pass = $Everything::CONF->{'everypass'};
+my $host = $Everything::CONF->{'everything_dbserv'};
+my $db = "everything";
+
+my $time = [gmtime()];
+my $year = $time->[5] + 1900;
+my $mon = sprintf("%02d", $time->[4] + 1);
+my $day = sprintf("%02d", $time->[3]);
+
+if($pass ne "")
+{
+	$pass = "-p$pass";
+}
+
+my $filename = "everything.$year$mon$day.sql.gz";
+
+`mysqldump -u$user --single-transaction --routines --triggers $pass -h$host $db | gzip --best > "$tmpdir/$filename"`;
+if($uploader->upload_file($filename, "$tmpdir/$filename"))
+{
+	if($Everything::CONF->{notification_email} ne "")
+	{
+		my $email = getNode("backup ready mail", "mail");
+		$email->{doctext} =~ s/\<filename\>/$filename/g;
+		$email->{title} = "Backup ready on S3: $filename";
+		node2mail($Everything::CONF->{notification_email},$email,1);
+	}
+}
+
+`rm -rf $tmpdir`;
