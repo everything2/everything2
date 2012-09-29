ddf1dac968ff29166503046c1d3a74af24a92782
Jay Bonci <jay@bonci.net>
2012-09-29 13:09:32 -0400

    Properly log the e2cron entries into /var/log/everything

--- vagrant/cookbooks/e2cron/recipes/default.rb
+++ vagrant/cookbooks/e2cron/recipes/default.rb
@@ -8,7 +8,7 @@
 #
 
 logdir = "/var/log/everything"
-datelog = "`date +\\%Y\\%m\\%H`.log"
+datelog = "`date +\\%Y\\%m\\%d\\%H`.log"
 
 directory logdir do
   owner "root"
@@ -20,44 +20,44 @@ end
 cron 'database_backup_to_s3.pl' do
   hour "0"
   minute "2"
-  command "/var/everything/tools/database_backup_to_s3.pl &>> #{logdir}/e2cron.database_backup_to_s3.#{datelog}" 
+  command "/var/everything/tools/database_backup_to_s3.pl 2>&1 >> #{logdir}/e2cron.database_backup_to_s3.#{datelog}" 
 end
 
 cron 'updateNodelet.pl' do
   user "root"
   minute "0-59/5"
-  command "/var/everything/ecore/bin/updateNodelet.pl &>> #{logdir}/e2cron.updateNodelet.#{datelog}"
+  command "/var/everything/ecore/bin/updateNodelet.pl 2>&1 >> #{logdir}/e2cron.updateNodelet.#{datelog}"
 end
 
 cron 'refreshRoom.pl' do
   user "root"
   minute "0-59/5"
-  command "/var/everything/ecore/bin/refreshRoom.pl &>> #{logdir}/e2cron.refreshRoom.#{datelog}"
+  command "/var/everything/ecore/bin/refreshRoom.pl 2>&1 >> #{logdir}/e2cron.refreshRoom.#{datelog}"
 end
 
 cron 'cleanCbox.pl' do
   user "root"
   minute "50"
-  command "/var/everything/ecore/bin/cleanCbox.pl &>> #{logdir}/e2cron.cleanCbox.#{datelog}"
+  command "/var/everything/ecore/bin/cleanCbox.pl 2>&1 >> #{logdir}/e2cron.cleanCbox.#{datelog}"
 end
 
 cron 'newstats.pl' do
   user "root"
   minute "10"
   hour "6"
-  command "/var/everything/ecore/bin/newstats.pl &>> #{logdir}/e2cron.newstats.#{datelog}"
+  command "/var/everything/ecore/bin/newstats.pl 2>&1 >> #{logdir}/e2cron.newstats.#{datelog}"
 end
 
 cron 'expirerooms.pl' do
   user "root"
   minute "30"
   hour "6"
-  command "/var/everything/ecore/bin/expirerooms.pl &>> #{logdir}/e2cron.expirerooms.#{datelog}"
+  command "/var/everything/ecore/bin/expirerooms.pl 2>&1 >> #{logdir}/e2cron.expirerooms.#{datelog}"
 end
 
 cron 'reaper.pl' do
   user "root"
   minute "50"
   hour "6"
-  command "/var/everything/ecore/bin/reaper.pl &>> #{logdir}/e2cron.reaper.#{datelog}"
+  command "/var/everything/ecore/bin/reaper.pl 2>&1 >> #{logdir}/e2cron.reaper.#{datelog}"
 end
