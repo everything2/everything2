382e420856f8145e4ceb11dec7136973d331506a
Jay Bonci <jay@bonci.net>
2012-11-28 01:58:24 +0000

    Add in a dataprovider for nodeparam

---
+++ ecore/Everything/dataprovider/nodeparam.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(lib);
+use Everything::dataprovider::base;
+package Everything::dataprovider::nodeparam;
+use base qw(Everything::dataprovider::base);
+
+sub data_out
+{
+	my ($this, $nodeidhash) = @_;
+
+	my $inclause = join(",",keys %$nodeidhash);
+
+	my $csr = $this->{dbh}->prepare("select * from nodeparam where node_id IN($inclause)");
+	$csr->execute();
+	my $data;
+	while(my $row = $csr->fetchrow_hashref())
+	{
+		push @{$data->{nodeparams}}, $row;
+	}
+	
+	return $this->SUPER::xml_out($data);
+}
+
+sub data_in
+{
+	my ($this, $xml) = @_;
+	my $data = $this->{xs}->XMLin($xml);
+	foreach my $nodeparam (@{$data->{nodeparams}->{nodeparam}})
+	{
+		$this->_hash_insert("nodeparam",$nodeparam);
+	}
+}
+
+1;
