1001a40f1e4800edf6c2244b6fd66813bf143cab
Jay Bonci <jay@bonci.net>
2013-01-28 23:53:45 -0500

    Added e2webexperiemental stuff for moose/mason

---
+++ vagrant/cookbooks/e2webexperimental/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: e2webexperimental
+# Recipe:: default
+#
+# Copyright 2013, Everything2 Media LLC
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'libhtml-mason-perl-doc' #HTML::Mason documentation
+package 'libhtml-mason-perl-examples' #HTML::Mason example setup
+package 'libhtml-mason-perl' #HTML::Mason Perl module
+package 'libmoose-perl' #modern Perl object system framework
