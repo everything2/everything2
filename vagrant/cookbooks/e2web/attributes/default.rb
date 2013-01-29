91d6a294c18e3ca2c44cfbf4e46e8de77995ec83
Jay Bonci <jay@bonci.net>
2013-01-28 22:03:40 -0500

    Up MaxClients

--- vagrant/cookbooks/e2web/attributes/default.rb
+++ vagrant/cookbooks/e2web/attributes/default.rb
@@ -1,4 +1,4 @@
-default["e2web"]["maxclients"]      = "35"
+default["e2web"]["maxclients"]      = "50"
 default["e2web"]["startservers"]    = "20"
 default["e2web"]["minspareservers"] = "10" 
 default["e2web"]["maxspareservers"] = "20"
