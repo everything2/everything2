86e65ff1cc367852787c6bd7a15377e75eb822eb
Jay Bonci <jay@bonci.net>
2013-02-07 05:26:40 +0000

    Take out the must-revalidate pragma as we have things versioned and it is sending IMS requests

--- ecore/Everything/Application.pm
+++ ecore/Everything/Application.pm
@@ -1778,7 +1778,7 @@ sub uploadS3Content
 			$properties->{content_encoding} = 'gzip';			
 		}
 
-		$properties->{cache_control} = "max-age=604800, must-revalidate"; #one week
+		$properties->{cache_control} = "max-age=604800"; #one week
 
 		$s3->upload_file($filespec->[0], $filespec->[0], $properties);
 	}
