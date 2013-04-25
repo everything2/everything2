deba39d103cc16423d4457807cf668294aadb6fc
Jay Bonci <jay@bonci.net>
2013-04-25 01:36:38 -0400

    Move over the rest of bin now that everything is accounted for

---
+++ tools/lfcalc.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib "/var/everything/ecore";
+use Everything;
+use Everything::HTML;
+initEverything 'everything';
+
+
+my $hrstats = getNode("hrstats", "setting");
+my $hrv = getVars($hrstats);
+$$hrv{mean} =sprintf("%.4f", $DB->sqlSelect("AVG(merit)", "user", "numwriteups>=25"));
+$$hrv{stddev} = sprintf("%.4f",$DB->sqlSelect("STD(merit)","user", "numwriteups>=25"));
+setVars($hrstats, $hrv);
+updateNode($hrstats, -1);
