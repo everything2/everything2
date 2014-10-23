83af068f897983a6e9ab0d637993b2c2d443f9bb
Jay Bonci <jay@bonci.net>
2014-10-23 01:48:38 +0000

    Production import; specifically add in IE fixes and uncloak object model fixes

---
+++ tools/stripvar.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(../ecore);
+use Everything;
+
+initEverything 'everything';
+
+my $csr = $DB->sqlSelectMany('user_id','user');
+my $type = getType('user');
+my $varstats;
+my $specialstats;
+
+my $tostrip = "ekw_logobackground";
+
+while(my $row = $csr->fetchrow_hashref())
+{
+	my $N = getNodeById($row->{user_id});
+	unless($N)
+	{
+		print "Bad data in user table: '".$row->{user_id}."'\n";
+		next;
+	}
+
+	if($N->{type_nodetype} != $type->{node_id})
+	{
+		print "Strange data mismatch with stuff from user: '$$N{node_id}'\n";
+		next;
+	}
+	my $v = getVars($N);
+	print "Inspecting: $$N{title}\n";
+	next unless exists $v->{$tostrip};
+	print "$$N{title} has '$tostrip'\n";
+	delete $v->{$tostrip};
+	setVars($N,$v);
+	$DB->updateNode($N,-1);
+	#exit;
+}
