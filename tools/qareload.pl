2d63f4a0a77a72bc50ab988373e7b357f1e5f14a
Jay Bonci <jay@bonci.net>
2014-11-30 18:03:57 +0000

    Do the reload the data stash items and put the seeds in as a part of qareload.pl

--- tools/qareload.pl
+++ tools/qareload.pl
@@ -13,4 +13,6 @@ if($Everything::CONF->environment ne "development")
 `echo "drop database everything" | mysql -u root`;
 `echo "create database everything" | mysql -u root`;
 `/var/everything/ecoretool/ecoretool.pl bootstrap --nodepack=/var/everything/nodepack`;
+`/var/everything/tools/seeds.pl`;
+`/var/everything/tools/data_generator_heartbeat.pl`;
 `/etc/init.d/apache2 restart`;
