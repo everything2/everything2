409c7138d2540c9659925f4d63f23af1377498e4
Jay Bonci <jay@bonci.net>
2014-12-02 01:08:22 +0000

    Add two more generators for coolnodes and creamofthecool

---
+++ ecore/Everything/DataStash/coolnodes.pm
@@ -0,0 +1,21 @@
+package Everything::DataStash::coolnodes;
+
+use Moose;
+use namespace::autoclean;
+extends 'Everything::DataStash';
+
+has '+interval' => (default => 60);
+
+sub generate
+{
+  my ($this) = @_;
+
+  my $cuss = $this->DB->sqlSelectMany("(select title from node where node_id=cooledby_user limit 1) as cooluser, coolwriteups_id, (select title from node where node_id=(select author_user from node where node_id=coolwriteups_id limit 1) limit 1) as wu_author, (select parent_e2node from writeup where writeup_id=coolwriteups_id limit 1) as parentNode, (select title from node where node_id=parentNode limit 1) as parentTitle, (select cooled from writeup where writeup_id=coolwriteups_id limit 1) as writeupCooled", "coolwriteups", "", "order by tstamp desc limit 30");
+
+
+  return $this->SUPER::generate($cuss->fetchall_arrayref({}));
+}
+
+
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ ecore/Everything/DataStash/creamofthecool.pm
@@ -0,0 +1,40 @@
+package Everything::DataStash::creamofthecool;
+
+use Moose;
+use namespace::autoclean;
+extends 'Everything::DataStash';
+
+has '+interval' => (default => 60);
+
+sub generate
+{
+  my ($this) = @_;
+  my $repthreshholdlo = 12;
+  my $repthreshholdhi = 20;
+  my $limit = 4;
+  my $length = 512;
+
+  my $cuss = $this->DB->sqlSelectMany(
+	'wu.node_id, wu.author_user,
+		parent_e2node,
+		doctext,
+		type.title as type_title'
+	,
+	'node wu
+		JOIN writeup ON writeup_id= wu.node_id 
+		JOIN document on document_id = wu.node_id
+		JOIN node type on type.node_id = writeup.wrtype_writeuptype'
+	,
+	"wu.reputation > $repthreshholdlo
+		AND wu.reputation < $repthreshholdhi
+		AND writeup.cooled != 0"
+	,
+	"ORDER BY writeup.publishtime DESC LIMIT $limit"
+);
+
+  return $this->SUPER::generate($cuss->fetchall_arrayref({}));
+}
+
+
+__PACKAGE__->meta->make_immutable;
+1;
