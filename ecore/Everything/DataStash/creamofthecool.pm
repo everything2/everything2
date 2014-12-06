e99725b9000ab3e002faeff7ef8654c0da769fee
Jay Bonci <jay@bonci.net>
2014-12-06 19:15:30 +0000

    Fix spacing on creamofthecool

--- ecore/Everything/DataStash/creamofthecool.pm
+++ ecore/Everything/DataStash/creamofthecool.pm
@@ -9,27 +9,24 @@ has '+interval' => (default => 60);
 sub generate
 {
   my ($this) = @_;
-  my $repthreshholdlo = 12;
-  my $repthreshholdhi = 20;
-  my $limit = 4;
-  my $length = 512;
 
-  my $cuss = $this->DB->sqlSelectMany(
-	'wu.node_id, wu.author_user,
-		parent_e2node,
-		doctext,
-		type.title as type_title'
-	,
-	'node wu
-		JOIN writeup ON writeup_id= wu.node_id 
-		JOIN document on document_id = wu.node_id
-		JOIN node type on type.node_id = writeup.wrtype_writeuptype'
-	,
-	"wu.reputation > $repthreshholdlo
-		AND wu.reputation < $repthreshholdhi
-		AND writeup.cooled != 0"
-	,
-	"ORDER BY writeup.publishtime DESC LIMIT $limit"
+my $repthreshholdlo = 12;
+my $repthreshholdhi = 20;
+my $limit = 4;
+
+my $cuss = $this->DB->sqlSelectMany(
+  'wu.node_id, wu.author_user,
+    parent_e2node,
+    doctext,
+    type.title as type_title',
+  'node wu
+    JOIN writeup ON writeup_id= wu.node_id 
+    JOIN document on document_id = wu.node_id
+    JOIN node type on type.node_id = writeup.wrtype_writeuptype',
+  "wu.reputation > $repthreshholdlo
+    AND wu.reputation < $repthreshholdhi
+    AND writeup.cooled != 0",
+  "ORDER BY writeup.publishtime DESC LIMIT $limit"
 );
 
   return $this->SUPER::generate($cuss->fetchall_arrayref({}));
