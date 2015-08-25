4e62b566c6b16e4a2feb6f1c8574554e25cffb8a
Jay Bonci <jay@bonci.net>
2015-08-25 08:37:35 -0400

    Updates to e2web cookbook to bump version

--- vagrant/cookbooks/e2web/metadata.rb
+++ vagrant/cookbooks/e2web/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "jay@bonci.net"
 license          "Same as the Everything Engine"
 description      "Installs/Configures e2web"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
