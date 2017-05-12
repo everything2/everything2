130b2fd0dc1cde31b37ea47a22b2aee9af7a17f8
Jay <jay@bonci.net>
2017-05-12 09:46:34 -0400

    Pull all of the nodelet code out of the database and into PMs
    Fixes #426

--- ecore/Everything/Delegation/nodelet.pm
+++ ecore/Everything/Delegation/nodelet.pm
@@ -282,7 +282,7 @@ sub other_users
       my $costume = $$userVars{costume};
       if (defined $costume and $costume ne '')
       {
-        $costume = encodeHTML($$userVars{costume}, $bAndBrackets);
+        $costume = $APP->encodeHTML($$userVars{costume}, $bAndBrackets);
         $nameLink = linkNode($userID, $costume);
       }
     }
@@ -783,7 +783,7 @@ sub random_nodes
   return $str;
 }
 
-sub tips
+sub everything_developer
 {
   my $DB = shift;
   my $query = shift;
@@ -794,23 +794,633 @@ sub tips
   my $APP = shift;
 
   my $str = "";
-  $str .= qq|<hr width=%70>|;
 
-  my @tips = ("[Avoid highly subjective writeups]",
-   "[Earn Your Bullshit]",
-   "[Please use plain text]",
-   "[Link and Link]",
-   "[Integrate Your Write-Ups]",
-   "[Don't Namespace Your Lyrics]",
-   "[Pick Titles Carefully]",
-   "[Please Attribute Anything You Quote]",
-   "[Cut-and-Paste Writeups Will Die]",
-   "(Here's) [How to Format Poems and Simple HTML]",
-   "[Node For The Ages]");
+  my $vc = htmlcode('viewcode');
+  my @links = ();
 
-  $str .= "<p align=center>".$tips[rand(int(@tips))].qq|<hr width=%70>|;
+  push @links, $vc if $vc;
+  push @links, '<font size="1">'.linkNode($NODE, 'xmltrue', {displaytype=>'xmltrue'}).'</font>';
+  $str .= join ' / ',@links;
+  $str .= qq|<br />|;
 
-  return $str."TESTING";
+  if($$VARS{nodelets})
+  {
+    $str .= 'node_id='.getId($NODE). ' (' . $$NODE{type}{title} . ') <small>' . $$NODE{createtime} . ' ('.htmlcode('timesince',$$NODE{createtime}.',1')." old)</small><br />\n";
+
+    $str .='<font size="1">using: '.linkNode(getPage($NODE, $query->param('displaytype'))).'</font><br /><br />';
+    $str .= '<a href='.urlGen({node=>'edev',type=>'usergroup'}).'>edev usergroup</a>'.
+    '<br />'.
+    htmlcode('nodeletsection','edn,cgiparam').
+    htmlcode('nodeletsection','edn,edev').
+    htmlcode('nodeletsection','edn,util').
+    htmlcode('nodeletsection','edn,globals').
+    htmlcode('nodeletsection','edn,patches');
+  }
+
+  return $str;
+}
+
+sub statistics
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  $$VARS{numwriteups} ||=0;
+  $$VARS{IQM} ||=0;
+
+  my $str = "";
+  $str .= htmlcode('nodeletsection','edn,cgiparam');
+  $str .= htmlcode('nodeletsection','stat,personal,Yours,,,i');
+  $str .= htmlcode('nodeletsection','stat,fun,Fun Stats,,,i');
+  $str .= htmlcode('nodeletsection','stat,advancement,Old Merit System,,,i'); 
+
+  return $str;
+}
+
+sub readthis
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = "";
+  $str .= htmlcode('nodeletsection','rtn,cwu,Cool Writeups,Cool Archive,,i');
+  $str .= htmlcode('nodeletsection','rtn,edc,Editor Selections,Page of Cool,,i');
+  $str .= htmlcode('nodeletsection','rtn,nws,News,,,i');
+
+  return $str;
+}
+
+sub notelet
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = "";
+
+  if((exists $VARS->{lockCustomHTML}) || (exists $VARS->{noteletLocked}))
+  {
+    #latter way is deprecated
+    #may later say why locked? or give a msg field to anon admin?
+    return 'Sorry, your Notelet is currently locked, probably because an administrator is working with your account. It should soon be back to normal.';
+  }
+
+  #message to display if text not set
+  #(urlGen instead parseLinks to foil pranksters making e2nodes with same title)
+  my $blankMsg = 'You currently have no text set for your personal nodelet. You can edit it at <a href='.
+    urlGen({'node'=>'Notelet Editor','type'=>'superdoc'}).'>Notelet Editor</a> or <br>'.
+    linkNode($NODE, 'remove it here', {
+      op => 'movenodelet',
+      position => 'x',
+      nodelet => 'Notelet',
+      -id => 'noteletremovallink',
+      -class => 'ajax (noteletremovallink):ajaxEcho:'
+      .q!remove+it+here!
+      .q!&lt;script+type='text/javascript'&gt;!
+      .q!e2.vanish($('#notelet'));&lt/script&gt;!
+      }).' or at your <a href='.urlGen({'node'=>'Nodelet Settings','type'=>'superdoc'}).'>Nodelet Settings</a>';
+
+  #have to deal with deprecated stuff
+  if(exists $VARS->{personalRaw})
+  {
+    $VARS->{noteletRaw} = $VARS->{personalRaw};
+    delete $VARS->{personalRaw};
+  }
+
+  if(exists $VARS->{personalScreened})
+  {
+    $VARS->{noteletScreened} = $VARS->{personalScreened};
+    delete $VARS->{personalScreened};
+  }
+
+  unless ((exists $VARS->{'noteletRaw'}) && length($VARS->{'noteletRaw'}))
+  {
+    $str .= $blankMsg;
+  }else{
+    unless(exists $VARS->{'noteletScreened'})
+    {
+      htmlcode('screenNotelet','');
+      $str .= $blankMsg unless ((exists $VARS->{'noteletRaw'}) && length($VARS->{'noteletRaw'}));
+    }else{
+      $str .= parseLinks($VARS->{'noteletScreened'});
+    }
+  }
+
+  $str .=qq|<div class="nodeletfoot">(|.linkNode(getNode('Notelet editor','superdoc'),'edit').qq|)</div>|;
+  return $str;
+}
+
+sub recent_nodes
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = "";
+
+  $$VARS{nodetrail} ||= "";
+  my @list = split(",",$$VARS{nodetrail});
+  #put this node_id at the top of the list for next time
+  $$VARS{nodetrail} = $$NODE{node_id} . ',';
+
+  return '<small><em>All forgotten...</em></small>' if $query->param('eraseTrail');
+
+  my @sayings = ("A trail of crumbs", "Footprints in the sand",
+		"Are we there yet?", "A snapshot...", "The ghost of nodes past");
+
+  $str = '<em>'.$sayings[(rand(@sayings))].'</em>';
+  return unless scalar @list;
+
+  my $i=0;
+  my $list = '';
+  foreach (@list) {
+    next unless $_;
+    next unless $$VARS{nodetrail} !~ /\b$_\b/; 	#skip dupes
+    $list .= "<li>" . linkNode(getNodeById($_), undef, {"lastnode_id" => 0}) . "</li>";
+    $$VARS{nodetrail} .= $_ . ',' ; #push this onto the bottom of the list
+    last if ++$i > 8;
+  }
+  my @quotes = ("Cover my tracks", "Deny my past", "The Feds are knocking", "Wipe the slate clean");
+	$str .= ":\n<ol>$list\n</ol>\n".
+		htmlcode('openform', -class => 'nodeletfoot').
+		$query -> hidden('eraseTrail','1').
+		$query -> submit('schwammdrueber', $quotes[(rand(@quotes))]).
+	'</form>' if $list;
+
+  return $str;
+
+}
+
+sub master_control
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = "";
+
+  my $UID = $$USER{user_id};
+  my $isAdmin = $APP->isAdmin($USER);
+  my $isCE = $APP->isEditor($USER);
+
+  return 'Nothing for you here.' unless $isCE;
+  $str = htmlcode('admin searchform');
+  $str .= htmlcode('admin toolset') if $isAdmin;
+  $str .= htmlcode('nodenote');
+  $str .= htmlcode('nodeletsection', 'epi', 'admins', 'Admin') if $isAdmin;
+  $str .= htmlcode('nodeletsection', 'epi', 'ces', 'CE');
+  return $str;
+
+}
+
+sub current_user_poll
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  return htmlcode('showcurrentpoll',"in nodelet");
+}
+
+sub favorite_noders
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  $$VARS{favorite_limit} = int($query->param("fave_limit")) if (int($query->param("fave_limit")));
+
+  my $wuLimit = int($$VARS{favorite_limit}) || 15;
+  $wuLimit = 50 if ($wuLimit > 50 || $wuLimit < 1);
+
+  my $linktypeFavorite = getNode('favorite', 'linktype');
+  return '' unless $linktypeFavorite;
+  my $linktypeIdFavorite = $$linktypeFavorite{node_id};
+  my $typeIdWriteup = getType('writeup')->{node_id};
+  my $str = '';
+
+  $str .= htmlcode('openform'). '<label>Limit: '. $query->textfield(-name => 'fave_limit', -size => 3 )
+  . '</lable>'
+  . $query->submit("sexisgood","update")
+  . $query->end_form();
+
+  my $queryStringFavorites = <<SQLEND;
+    SELECT node.node_id, node.author_user
+    FROM links
+    JOIN node
+      ON links.to_node = node.author_user
+    WHERE links.linktype = $linktypeIdFavorite
+      AND links.from_node = $$USER{user_id}
+      AND node.type_nodetype = $typeIdWriteup
+    ORDER BY node_id
+    DESC LIMIT $wuLimit
+SQLEND
+
+  my $writeuplist = $DB->getDatabaseHandle()->selectall_arrayref($queryStringFavorites);
+
+  my $wuListText = "<ul id='writeup_faves'>";
+
+  for my $n (@$writeuplist) {
+    my $N = getNodeById($$n[0]);
+    $wuListText .=
+      "<li><span class='writeupmeta'><span class='title'>".linkNode($$N{node_id})."</span> "
+      . "by <span class='author'>".linkNode($$N{author_user})."</span></span></li>";
+  }
+
+  $wuListText .="</ul>";
+  return $str . $wuListText;
+}
+
+sub new_logs
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+
+  my $str = htmlcode('daylog').htmlcode('zenwriteups',"New Logs");
+  $str .= qq|<div class="nodeletfoot morelink">|;
+  $str .= linkNode(getNode('Writeups by Type', 'superdoc'), 'more', {wutype=>getNode('log', 'writeuptype')->{node_id}});
+  $str .= qq|</div>|;
+
+  return $str;
+}
+
+sub usergroup_writeups
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $isEd = $APP->isEditor($USER);
+  my $webloggables = getVars(getNode("webloggables", "setting"));
+  my $view_weblog ="";
+  # It might be more efficient to use a node_id for the VARS, but I'm not sure it'd make enough difference to be worth changing.
+  my $ug=getNode("$$VARS{nodeletusergroup}"||"E2science","usergroup");
+  $view_weblog=getId($ug);
+
+  # Most of the next chunk of code is pulled from [news archives].
+
+  my @labels = ();
+  my $counter = 0;
+  my $limit = 14;
+
+  my @groups = ();
+
+
+
+  foreach my $node_id (sort {
+        lc($$webloggables{$a}) cmp lc($$webloggables{$b})
+      } keys(%$webloggables)){
+    my $title =  $$webloggables{$node_id};
+    my $wclause = "weblog_id='$node_id' AND removedby_user=''";
+    my $count = $DB->sqlSelect('count(*)','weblog',"$wclause");
+    my $link = linkNode($NODE,$title,{'view_weblog'=>"$node_id"});
+    $link = "<b>$link</b>" if $node_id == $view_weblog;
+    push @labels, "$link<br><small>($count node".
+    ($count==1?'':'s').')</small>';
+  }
+
+  my $text = "";
+
+  return $text if (($view_weblog == 114)||($view_weblog==923653))&&(!($isEd));
+
+  $text .= '<p align="center">'.
+           linkNode($ug).' writeups</p>';
+
+  $text .= '<ul class="linklist">';
+  my $wclause = "weblog_id='$view_weblog' AND removedby_user=''";
+  my $csr = $DB->sqlSelectMany('*','weblog',$wclause,'order by tstamp desc');
+  while(($counter <= $limit) && (my $ref = $csr->fetchrow_hashref())){
+    my $N = getNode($$ref{to_node});
+    next unless $N;
+    my $link = linkNode($N);
+    $text .= "<li>$link</li>\n";
+    $counter++;
+  }
+  $text .= "</ul>";
+
+
+  # Pull a list of node_ids for groups the user can weblog to:
+
+  my $can_weblog=$$VARS{can_weblog};
+  my @groupids=split(',', $can_weblog);
+  # Put in a menu so the user can choose different groups:
+
+  $$VARS{nodeletusergroup} = 'E2science' unless $$VARS{nodeletusergroup};
+
+  my $str = htmlcode('openform');
+
+  $str.="\n\t<input type='hidden' name='op' value='changeusergroup'>";
+  $str.="\n\t<select name='newusergroup' class='ajax usergroupwriteups:updateNodelet?op=/op&newusergroup=/newusergroup:Usergroup Writeups'>";
+
+  # get the titles of all the usergroups we need. I bet there's a more efficient way of doing this.
+  for(@groupids)
+  {
+    next if ($_ == 165580); # Exclude 'News for Noders. Stuff that matters.'
+    push (@groups, getNodeById($_,"light")->{title});
+  }
+
+  for(@groups)
+  {
+    $str.="\n\t\t<option value=\"$_\"";
+    $str.=' selected="selected"' if $_ eq $$VARS{nodeletusergroup};
+    $str.=">$_</option>";
+  }
+  $str.="\n\t</select>";
+  $str.="\n\t".$query->submit("sexisgood","show");
+  $str.="\n".$query->end_form;
+  $text.=$str;
+
+  return $text;
+
+}
+
+sub notifications
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = qq|<ul id='notifications_list'>|;
+  # show settings dialog if no notifications active
+  $query -> param('showwidget', 'notificationssettings') unless $$VARS{settings} || ($$NODE{title} eq 'Nodelet Settings' && $$NODE{type}{title} eq 'superdoc');
+  # do this here to update settings before showing nodelet:
+  my $settingslink = htmlcode('nodeletsettingswidget','Notifications', 'Notification settings');
+
+  my $notification_list = htmlcode('notificationsJSON', 'wrap'); # 'wrap' to get markup for list
+my $notify_count = 1;
+
+  while (defined $$notification_list{$notify_count}) {
+    my $notify = $$notification_list{$notify_count};
+    $str .= "$$notify{value}\n";
+    $notify_count++;
+  }
+
+return $str.qq'\n</ul>\n<div class="nodeletfoot">\n$settingslink\n</div>';
+
+}
+
+sub everything_most_wanted
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $wantedLinktype = getNode("mostwanted","linktype")->{node_id};
+  my $mostWanted = $DB->sqlSelectMany("to_node, from_node","links","linktype=$wantedLinktype limit 6");
+
+  my $wantedStr = "";
+  my $wCount = 0;
+
+  while (my $w = $mostWanted->fetchrow_hashref)
+  {
+    $wCount++;
+    $wantedStr .="<p>".linkNode($$w{from_node})." will pay 50XP to see <b>".linkNode($$w{to_node})."</b> filled.</p>";
+    last if $wCount == 3;
+  }
+
+  $wantedStr .= "<p>Know anything about ... </p>";
+  return $wantedStr;
+}
+
+sub categories
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $GU = $Everything::CONF->guest_user;
+  my $uid = $$USER{user_id};
+
+  my $str = "";
+
+  my $sql = "SELECT DISTINCT ug.node_id
+    FROM node ug,nodegroup ng 
+    WHERE ng.nodegroup_id=ug.node_id AND ng.node_id=$uid";
+
+  my $ds = $DB->{dbh}->prepare($sql);
+  $ds->execute() or return $ds->errstr;
+  my $inClause = $uid.','.$GU;
+  while(my $n = $ds->fetchrow_hashref)
+  {
+    $inClause .= ','.$$n{node_id};
+  }
+
+  # Now get all the categories the user can edit
+  $sql = "SELECT n.node_id, n.author_user
+    FROM node n
+    WHERE author_user IN ($inClause)
+    AND type_nodetype=1522375
+    AND  node_id NOT IN (SELECT to_node AS node_id FROM links WHERE from_node=n.node_id)
+    ORDER BY n.title";
+
+  $ds = $DB->{dbh}->prepare($sql);
+  $ds->execute() or return $ds->errstr;
+  my $ctr = 0;
+  my $strList = "";
+  while(my $n = $ds->fetchrow_hashref)
+  {
+    $ctr++;
+    $strList .= '<li>'.linkNode($$n{node_id}, '', {lastnode_id=>0})
+      .' by '.linkNode($$n{author_user}, '', {lastnode_id=>0})
+      .' (<a href="/index.pl?op=category&node_id='.$$NODE{node_id}.'&cid='.$$n{node_id}.'&nid='.$$NODE{node_id}.'">add</a>)</li>';
+  }
+
+  $str .= '<ul id="nodelists">'
+         .$strList
+         .'</ul>
+         <div class="nodeletfoot">'
+        .linkNodeTitle('Create Category', 0, 'Add a new Category')
+        .'</div>';
+
+  return $str;
+
+}
+
+sub most_wanted
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $str = qq|<table>|;
+
+  $str.="<p><table class='mytable'><tr><th>Requesting Sheriff</th><th>Outlaw Nodeshell</th><th>GP Reward (if any)</th></tr>";
+
+  my $REQ = getVars(getNode('bounty order','setting'));
+  my $OUT = getVars(getNode('outlaws', 'setting'));
+  my $REW = getVars(getNode('bounties', 'setting'));
+  my $HIGH = getVars(getNode('bounty number', 'setting'));
+  my $MAX = 5;
+
+  my $bountyTot = $$HIGH{1};
+  my $numberShown = 0;
+  my $outlawStr = "";
+  my $requester = "";
+  my $reward = "";
+
+  for(my $i = $bountyTot; $numberShown < $MAX; $i--)
+  {
+
+    if (exists $$REQ{$i})
+    {
+      $numberShown++;
+      my $requesterName = $$REQ{$i};
+      $requester = linkNode(getNode($requesterName, 'user'));
+      $outlawStr = parseLinks($APP->encodeHTML($$OUT{$requesterName}));
+      $reward = $APP->encodeHTML($$REW{$requesterName});
+      $str.="<tr><TD>$requester</TD><TD>$outlawStr</TD><TD>$reward</TD></tr>";
+    }
+  }
+
+  $str.="<p><small>Fill these nodes and get rewards! More details at " . linkNodeTitle('Everything\'s Most Wanted') . "\.</small></p>";
+  $str.=qq|</table>|;
+
+return $str;
+
+}
+
+sub messages
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  return qq|<div id="messages_messages">|.htmlcode('testshowmessages').qq|</div>|;
+
+}
+
+sub neglected_drafts
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my $setType = sub{
+    # make hashref look enough like a real node that linkNode works properly
+    $_[0] -> {type}{title} = 'draft';
+    '';
+  };
+
+  my $days = sub{
+    qq'<span class="days">&#91;$_[0]->{days} days&#93;</span>';
+  };
+
+  my $str = "";
+
+  my $data = $DB->stashData("neglecteddrafts");
+
+  foreach my $param (qw(Editor Author))
+  {
+    $str .= "<h4>$param neglect</h4>";
+    my $list = htmlcode('show content', $data->{lc($param)}, '<li> setType, title, byline, days', setType => $setType, days => $days);
+
+    if ($list){
+      $str .= qq'<ul class="infolist">$list</ul>' if $list;
+    }else{
+      $str .= '<p><small><em>(none)</em></small></p>';
+    }
+  }
+
+  return $str.qq|<div class="nodeletfoot"><a href="/node/superdoc/Drafts For Review">Drafts for Review</a></div>|;
+}
+
+sub for_review
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  my %funx = (
+    startline => sub{
+      $_[0] -> {type}{title} = 'draft';
+      '<td>';
+    },
+    notes => sub{
+      $_[0]{latestnote} =~ s/\[user\]//;
+      my $note = $APP->encodeHTML($_[0]{latestnote}, 'adv');
+      '<td align="center">'
+      .($_[0]{notecount} ? linkNode($_[0], $_[0]{notecount},
+      {'#' => 'nodenotes', -title => "$_[0]{notecount} notes; latest $note"})
+      : '&nbsp;')
+      .'</td>';
+      }
+  );
+
+  my $drafts = $DB->stashData("reviewdrafts");
+
+  return "<table><tr><th>Draft</th>".($APP->isEditor($USER)?(qq|<th align="center" title="node notes">N?</th>|):(""))."</tr>"
+    .htmlcode('show content', $drafts
+    , qq!<tr class="&oddrow"> startline, title, byline, "</td>",!.(($APP->isEditor($USER)?("notes"):(""))),%funx)
+    .'</table>';
+
 }
 
 1;
