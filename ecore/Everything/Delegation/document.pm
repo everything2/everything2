aaacea1a58c7b2e9f6145d807edb277c03d25844
Jay Bonci <jay@bonci.net>
2018-04-08 16:30:25 -0400

    Delegate [E2 Full Text Search]
    Fixes #1094

--- ecore/Everything/Delegation/document.pm
+++ ecore/Everything/Delegation/document.pm
@@ -3133,4 +3133,58 @@ sub e2_collaboration_nodes
   return $str;
 }
 
+sub e2_full_text_search
+{
+  my $DB = shift;
+  my $query = shift;
+  my $NODE = shift;
+  my $USER = shift;
+  my $VARS = shift;
+  my $PAGELOAD = shift;
+  my $APP = shift;
+
+  return qq|
+<script type="text/javascript">
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://www.google.com/cse/api/branding.css">');
+</script>
+<div class="cse-branding-right" style="background-color:#FFFFFF;color:#000000">
+  <div class="cse-branding-form">
+    <form action="" id="cse-search-box">
+      <div>
+        <input type="hidden" name="node_id" value="|.$$NODE{node_id}.qq|">
+        <input type="hidden" name="cx" value="017923811620760923756:pspyfx78im4">
+        <input type="hidden" name="cof" value="FORID:9">
+        <input type="hidden" name="ie" value="UTF-8">
+        <input type="text" name="q" size="31">
+        <input type="submit" name="sa" value="Search">
+      </div>
+    </form>
+  </div>
+  <div class="cse-branding-logo">
+    <img src="https://www.google.com/images/poweredby_transparent/poweredby_FFFFFF.gif" alt="Google">
+  </div>
+  <div class="cse-branding-text">
+    Custom Search
+  </div>
+</div>
+
+
+
+
+<!-- Google Search Result Snippet Begins -->
+<div id="cse-search-results"></div>
+<script type="text/javascript">
+  var googleSearchIframeName = "cse-search-results";
+  var googleSearchFormName = "cse-search-box";
+  var googleSearchFrameWidth = 600;
+  var googleSearchDomain = "www.google.com";
+  var googleSearchPath = "/cse";
+</script>
+<script type="text/javascript" src="https://www.google.com/afsonline/show_afs_search.js"></script>
+
+<!-- Google Search Result Snippet Ends -->
+|;
+
+}
+
 1;
