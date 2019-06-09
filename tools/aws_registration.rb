718cca11083c2e8aa1523ae2556046cd8d66b467
Jay Bonci <jay@bonci.net>
2019-06-09 11:39:43 -0400

    Adds the aws_registration script to the tools directory
    Fixes #1328

---
+++ tools/aws_registration.rb
@@ -0,0 +1,95 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'aws-sdk'
+require 'net/http'
+require 'json'
+require 'uri'
+require 'getoptlong'
+
+@instance_server = 'http://169.254.169.254/latest'
+
+def query_metadata(value)
+  uri = URI("#{@instance_server}/#{value}")
+  req = Net::HTTP::Get.new(uri)
+
+  begin 
+    res = Net::HTTP.start(uri.hostname, uri.port){ |http|
+      http.read_timeout(10)
+      http.request(req)
+    }
+
+    res.body.chomp
+
+  rescue Net::ReadTimeout, Net::OpenTimeout => e
+    puts "Can't get instance metadata: #{e.message}" 
+    exit
+  end
+end
+
+def instance_identity
+  JSON.parse(query_metadata('dynamic/instance-identity/document'))
+end
+
+def instance_region
+  instance_identity['region']
+end
+
+@rds = Aws::RDS::Client.new(region: instance_region)
+@elb = Aws::ElasticLoadBalancingV2::Client.new(region: instance_region)
+
+def instance_id
+  instance_identity['instanceId']
+end
+
+def mac_address
+  query_metadata('meta-data/network/interfaces/macs/').split("\n")[0].gsub!(%r{/$},'')
+end
+
+def public_address
+  query_metadata("meta-data/network/interfaces/macs/#{mac_address}/public-ipv4s").split("\n")[0]
+end
+
+def frontend_elb
+  @elb.describe_load_balancers.load_balancers.each do |elb|
+    @elb.describe_tags(resource_arns: [elb['load_balancer_arn']]).tag_descriptions[0]['tags'].each do |tag|
+      return elb if(tag['key'].eql? 'app' and tag['value'].eql? 'e2')
+    end
+  end
+  nil
+end
+
+def web_cluster_target_group
+  @elb.describe_target_groups.target_groups.each do |tg|
+    return tg if tg['protocol'].eql? 'HTTPS' and tg['load_balancer_arns'].include?(frontend_elb['load_balancer_arn'])
+  end
+  nil
+end
+
+def db_security_group_registration
+  begin
+    @rds.authorize_db_security_group_ingress(db_security_group_name: 'default', cidrip: "#{public_address}/32")
+  rescue Aws::RDS::Errors::AuthorizationAlreadyExists
+  end
+end
+
+def elb_target_group_registration
+  @elb.register_targets(target_group_arn: web_cluster_target_group['target_group_arn'], targets: [{id: instance_id}])
+end
+
+
+opts = GetoptLong.new(
+  [ '--db', GetoptLong::NO_ARGUMENT ],
+  [ '--elb', GetoptLong::NO_ARGUMENT ],
+)
+
+opts.each do |opt|
+  case opt
+    when '--db'
+      db_security_group_registration
+    when '--elb'
+      elb_target_registration
+  end
+end
+
+
