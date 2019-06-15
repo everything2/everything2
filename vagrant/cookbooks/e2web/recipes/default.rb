ebc4a6ac6732126dcd279ea784a859887a8293cd
Jay Bonci <jay@bonci.net>
2019-06-14 22:38:35 -0400

    Always generate self-signed-certs
    Fixes #1351

--- vagrant/cookbooks/e2web/recipes/default.rb
+++ vagrant/cookbooks/e2web/recipes/default.rb
@@ -87,22 +87,10 @@ template "#{confdir}/ssl.conf" do
   variables(node["e2web"])
 end
 
-
-if node["e2web"]["make_snakeoil_tls_cert"]
-  bash "Create E2 snakeoil certs" do
-    cwd "/tmp"
-    user "root"
-    creates "/etc/apache2/e2.key"
-    code <<-EOH
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -batch -keyout /etc/apache2/e2.key -out /etc/apache2/e2.cert -subj '/C=US/ST=MA/L=Maynard/O=Everything2.com/OU=edev/CN=vagranttest.everything2.com'
-    EOH
-  end
-end
-
-bash "Check S3 for certs" do
+bash "Generate self-signed certs" do
   cwd "/tmp"
   user "root"
-  code "/var/everything/tools/fetch_tls_keys.pl"
+  code "/var/everything/tools/generate-self-signed-cert.rb"
 end
 
 file '/etc/logrotate.d/apache2' do
@@ -140,12 +128,6 @@ end
 cron 'log_deliver_to_s3.pl' do
   minute '5'
   command "/var/everything/tools/log_deliver_to_s3.pl 2>&1 >> #{logdir}/e2cron.log_deliver_to_s3.#{datelog}"
-end
-
-cron 'check for new TLS cert' do
-  minute '0'
-  hour '3'
-  command "/var/everything/tools/fetch_tls_keys.pl 2>&1 >> #{logdir}/e2web.fetch_tls_keys.#{datelog}"
 end
 
 service 'apache2' do
