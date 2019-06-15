8fd9e5cb978053f664fb64807b5dadaa74b7ad63
Jay Bonci <jay@bonci.net>
2019-06-15 18:07:10 -0400

    Removes the e2tls cookbook
    Fixes #1362

--- vagrant/cookbooks/e2tls/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'e2tls'
-maintainer       'Everything2 Media LLC'
-maintainer_email 'jay@bonci.net'
-license          'Distributed under the same license as E2 itself'
-description      'Installs/Configures e2tls'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
