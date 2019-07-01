534a2b5ad18fcd26d89ee923b981068529d4c4e4
Jay Bonci <jay@bonci.net>
2019-07-01 03:41:02 +0000

    Removes the legacy pieces from the data generator for the frontpage
    Fixes #1469

--- ecore/Everything/DataStash/frontpagenews.pm
+++ ecore/Everything/DataStash/frontpagenews.pm
@@ -13,12 +13,6 @@ sub generate
   my $frontpage_superdoc = $this->DB->getNode("News", "usergroup");
   my $weblog_entries = $this->APP->fetch_weblog($frontpage_superdoc, 5);
   
-  if(scalar(@$weblog_entries) == 0)
-  {
-    $frontpage_superdoc = $this->DB->getNode("News for noders. Stuff that matters.","superdoc");
-    $weblog_entries = $this->APP->fetch_weblog($frontpage_superdoc, 5);
-  }
-
   return $this->SUPER::generate($weblog_entries);
 }
 
