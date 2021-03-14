0847728fd911db59049beef1568edcb87835250f
Jay Bonci <jay@bonci.net>
2021-03-14 17:25:55 +0000

    Expands conftest to make testing items easier
    Fixes #1843

--- tools/conftest.pl
+++ tools/conftest.pl
@@ -8,13 +8,15 @@ use Everything;
 initEverything 'everything';
 
 
-foreach my $item (qw(configfile configdir site_url guest_user basedir infected_ips default_style everyuser everypass everything_dbserv database cookiepass canonical_web_server homenode_image_host mail_from nodecache_size environment s3 static_nodetypes clean_search_words_aggressively search_row_limit logdirectory use_local_javascript site_name create_new_user default_guest_node maintenance_nodes))
+foreach my $item (qw(configfile configdir site_url guest_user basedir infected_ips default_style everyuser everypass everything_dbserv database cookiepass canonical_web_server homenode_image_host mail_from nodecache_size environment s3 static_nodetypes clean_search_words_aggressively search_row_limit logdirectory use_local_javascript site_name create_new_user default_guest_node maintenance_nodes permanent_cache nosearch_words))
 {
   if(UNIVERSAL::isa($Everything::CONF->$item,"ARRAY"))
   {
-    print "$item: ARRAY\n";
+    print "$item: ARRAY ";
+    print "[".join(",",@{$Everything::CONF->$item})."]\n";
   }elsif(UNIVERSAL::isa($Everything::CONF->$item,"HASH")){
-    print "$item: HASH\n";
+    print "$item: HASH ";
+    print "{".join(",",(map {"$_ => ".$Everything::CONF->$item->{$_}} keys %{$Everything::CONF->$item}))."}\n";
   }else{
     print "$item: ".$Everything::CONF->$item."\n";
   }
