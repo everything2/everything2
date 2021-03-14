6f349ab437de5eff1a72c716619d429e653a1dd0
Jay Bonci <jay@bonci.net>
2021-03-14 17:35:32 -0400

    Removes the google_ads_badnodes writing
    Fixes #1858

--- vagrant/cookbooks/e2engine/attributes/default.rb
+++ vagrant/cookbooks/e2engine/attributes/default.rb
@@ -2,5 +2,4 @@ default["e2engine"]["everything_dbserv"] = "localhost"
 default["e2engine"]["canonical_web_server"] = "development.everything2.com"
 default["e2engine"]["environment"] = "development"
 default["e2engine"]["use_local_javascript"] = 1;
-default["e2engine"]["google_ads_badwords"] = ["fuckfuck"]
 default["e2engine"]["google_ads_badnodes"] = ["123456"]
