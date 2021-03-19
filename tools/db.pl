b633333cd856e85fe29c9ef3575371b6792c8c51
Jay Bonci <jay@bonci.net>
2021-03-19 19:02:15 +0000

    Removes the db.rb file, adds a db.pl file
    Fixes #1862

---
+++ tools/db.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(/var/libraries/lib/perl5);
+use lib qw(/home/ubuntu/everything2/ecore);
+use Paws;
+use Data::Dumper;
+use JSON;
+
+my $service = Paws->service('SecretsManager', region => 'us-west-2');
+foreach my $secret(@{$service->ListSecrets->SecretList})
+{
+  if($secret->Name eq "E2DBMasterPassword")
+  {
+    my $secret = from_json($service->GetSecretValue(SecretId => $secret->ARN)->SecretString);
+    #print Data::Dumper->Dump([$secret]);
+    exec("mysql --user=\"$secret->{username}\" --password=\"$secret->{password}\" --host=\"$secret->{host}\"");
+  }
+}
+
