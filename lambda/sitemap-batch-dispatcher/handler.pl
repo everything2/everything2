6ce445514d387f3ee236476f4ecad7736f67cebb
Jay Bonci <jay@bonci.net>
2021-04-04 21:21:50 -0400

    Creates all of the new service endpoints and fixes a problem with the deployment script when run out of the ops directory
    Fixes #2021

--- lambda/sitemap-batch-dispatcher/handler.pl
+++ lambda/sitemap-batch-dispatcher/handler.pl
@@ -4,11 +4,11 @@ use strict;
 use lib qw(/var/libraries/lib/perl5);
 use lib qw(/var/everything/ecore);
 use Everything;
+use Net::Amazon::S3;
 use Everything::S3;
+use Data::Dumper;
 use JSON;
 use Paws;
-
-#initEverything 'everything';
 
 sub http_response
 {
@@ -23,26 +23,36 @@ sub lambda_handler
 {
   my ($event) = @_;
 
-  print "Initializing everything engine";
-#  print $Everything::CONF->everypass."\n";
-#  initEverything 'everything';
-  
-#  print "Starting up sitemap dispatcher\n";
-#  my $s3 = Everything::S3->new("sitemapdispatch");
+  print "Initializing Everything Engine\n";
+  initEverything 'everything';
 
-#  my $lambda = Paws->service('lambda', 'region' => $Everything::CONF->current_region);
+  my $nets3 =   Net::Amazon::S3->new(
+                {
+                        use_iam_role => 1,
+                        retry => 1,
+                        host => 's3-us-west-2.amazonaws.com'
+                });
 
-#  my $current_batch = 1;
-#  my $batches = $APP->sitemap_batches;
 
-#  my $batches = [['459692'],['2177337']];
-#  foreach my $batch(@$batches)
-#  {
-#    print "Uploading batch: $current_batch.json\n";
-#    $s3->upload_data("$current_batch.json", JSON->new->utf8->encode($batch), {"content_type" => "application/json"});
-#    $lambda->InvokeAsync('FunctionName' => 'sitemap-batch-processor', 'InvokeArgs' => JSON->new->utf8->encode({"batch" => $current_batch}));
-#    $current_batch++;
-#  }
+  print "Starting up sitemap dispatcher\n";
+  print "Net::S3: ".Data::Dumper->Dump([$nets3])."\n";
+  print "Conf: ".Data::Dumper->Dump([$Everything::CONF->s3])."\n";
+  my $s3 = Everything::S3->new("sitemapdispatch");
+  return http_response(200, "OK");
+
+  my $lambda = Paws->service('Lambda', 'region' => $Everything::CONF->current_region);
+
+  my $current_batch = 1;
+  #my $batches = $APP->sitemap_batches;
+
+  my $batches = [['459692'],['2177337']];
+  foreach my $batch(@$batches)
+  {
+    print "Uploading batch: $current_batch.json\n";
+    $s3->upload_data("$current_batch.json", JSON->new->utf8->encode($batch), {"content_type" => "application/json"});
+    $lambda->InvokeAsync('FunctionName' => 'sitemap-batch-processor', 'InvokeArgs' => JSON->new->utf8->encode({"batch" => $current_batch}));
+    $current_batch++;
+  }
 
   http_response(200, "OK");
 }
