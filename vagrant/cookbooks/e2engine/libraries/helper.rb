bae6fe8ba6483b4e6cec275631ed0fe662969e5d
Jay Bonci <jay@bonci.net>
2021-06-20 22:07:03 -0400

    Removes e2web tendrils
    Fixes #2103

--- vagrant/cookbooks/e2engine/libraries/helper.rb
+++ vagrant/cookbooks/e2engine/libraries/helper.rb
@@ -3,7 +3,6 @@ module E2
     Chef::Log.info("E2 chef debug / Recipe List: #{node.recipe_list}")
     Chef::Log.info("E2 chef debug / Runlist recipes: #{node.primary_runlist.recipe_names}")
     Chef::Log.info("E2 chef debug / Runlist: #{node.primary_runlist.to_s}")
-    return 1 if node.primary_runlist.include?('recipe[e2web]') or node.primary_runlist.include?('role[e2development]')
-    return nil
+    return 1
   end
 end
