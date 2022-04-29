d56bab702ccdca6f9b9ec5d909a803ba5f2970c1
Jay Bonci <jay@bonci.net>
2022-04-28 23:36:53 -0400

    Completely rebuilds the development environment to move away from the old vagrant crutch.
    The cycle is fast, easy, and takes advantage of native caching from Docker
    Only thing that is left is to change up the build system in AWS
    
    Also, prepped another item cutover
    
    Fixes #2574

---
+++ ecore/Everything/Page/your_insured_writeups.pm
@@ -0,0 +1,25 @@
+package Everything::Page::your_insured_writeups;
+
+use Moose;
+extends 'Everything::Page';
+with 'Everything::Security::StaffOnly';
+
+sub display
+{
+  my ($self, $REQUEST) = @_;
+
+  my $csr = $self->DB->sqlSelectMany('publish_id', 'publish', 'publisher='.$REQUEST->user->id);
+
+  my $writeups = [];
+  while(my $pubwu = $csr->fetchrow_hashref)
+  {
+    my $wu = $self->APP->node_by_id($pubwu->{publish_id});
+    push @$writeups, $wu if(defined($wu));
+  }
+
+  return { writeups => $writeups }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- tools/write_apache_config.rb
+++ tools/write_apache_config.rb
@@ -2,8 +2,21 @@
 
 require 'erb'
 
-files = [{template: '/var/everything/vagrant/cookbooks/e2engine/templates/default/apache2.conf.erb', file: '/etc/apache2/apache2.conf'},
-	 {template: '/var/everything/vagrant/cookbooks/e2engine/templates/default/everything.erb', file: '/etc/apache2/everything.conf'}]
+bootstrap = nil
+
+['/var/everything/vagrant/cookbooks/e2engine/templates/default','/var/bootstrap/etc'].each do |bootstrapdir|
+  if Dir.exist? bootstrapdir
+    bootstrap = bootstrapdir
+  end
+end
+
+if bootstrap.nil?
+  puts "Could not find a bootstrap directory"
+  exit
+end
+
+files = [{template: "#{bootstrap}/apache2.conf.erb", file: '/etc/apache2/apache2.conf'},
+	 {template: "#{bootstrap}/everything.erb", file: '/etc/apache2/everything.conf'}]
 
 files.each do |f|
   template = ERB.new(File.open(f[:template]).read)
