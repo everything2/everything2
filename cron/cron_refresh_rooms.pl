555f6a7e4067a4bc93d3da84b221249421b7bdc3
Jay Bonci <jay@bonci.net>
2022-05-07 01:33:38 -0400

    Moves the refresh rooms cron job to the organized directory
    Fixes #2659

--- cron/cron_refresh_rooms.pl
+++ cron/cron_refresh_rooms.pl
@@ -6,8 +6,10 @@ use lib qw(/var/libraries/lib/perl5);
 use Everything;
 initEverything 'everything';
 
+print "Starting room refresh\n";
 my $actions = $APP->refreshRoomUsers;
 foreach my $action (@$actions)
 {
   print "action - $action->{action}, room - $action->{room}, user - $action->{user}\n";
 }
+print "Finished room refresh\n";
