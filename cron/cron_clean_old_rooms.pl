b8dc8b782ab901e6754e264893b3c4e603c3f207
Jay Bonci <jay@bonci.net>
2022-05-07 13:28:55 -0400

    Adds the room cleanup script to the proper cron directory
    Fixes #2663

--- cron/cron_clean_old_rooms.pl
+++ cron/cron_clean_old_rooms.pl
@@ -6,4 +6,6 @@ use lib qw(/var/libraries/lib/perl5);
 use Everything;
 initEverything 'everything';
 
+print "Starting cleaning old rooms\n";
 $APP->clean_old_rooms;
+print "Finished cleaning old rooms\n";
