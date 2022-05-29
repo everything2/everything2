51801428baeda93270bc1fdeb983bcd3ebd4e3f0
Jay Bonci <jay@bonci.net>
2022-05-29 13:56:40 -0400

    Updates to asset pipeline to include CSS
    More to be done
    Fixes #2754

--- tools/asset_pipeline.rb
+++ tools/asset_pipeline.rb
@@ -28,15 +28,23 @@ puts "Using asset bucket: #{asset_bucket}"
 
 everydir = File.expand_path(__dir__ + "/..")
 
-javascript_assets = {}
+assets = {'js' => {}, 'css' => {}}
 
-Dir["#{everydir}/www/js/*"].each do |f|
-  basefile = File.basename(f)
-  javascript_assets[basefile] = {}
-  javascript_assets[basefile]['plain'] = File.open(f).read
-  javascript_assets[basefile]['min'] = `npx terser #{f}`
-  javascript_assets[basefile]['min_gz'] = Zlib::Deflate.deflate(javascript_assets[basefile]['min'])
-  puts "Minified #{basefile}"
+['js','css'].each do |asset_type|
+  Dir["#{everydir}/www/#{asset_type}/*"].each do |f|
+    basefile = File.basename(f)
+    assets[asset_type][basefile] = {}
+    assets[asset_type][basefile]['plain'] = File.open(f).read
+
+    if(asset_type.eql? 'js')
+      assets[asset_type][basefile]['min'] = `npx terser #{f}`
+    elsif(asset_type.eql? 'css')
+      assets[asset_type][basefile]['min'] = `npx clean-css-cli #{f}`
+    end
+
+    assets[asset_type][basefile]['min_gz'] = Zlib::Deflate.deflate(assets[asset_type][basefile]['min'])
+    puts "Minified #{basefile}"
+  end
 end
 
 git_history = []
@@ -50,37 +58,45 @@ current_rev = git_history[0]
 
 puts "Uploading to: #{current_rev}"
 
-javascript_assets.keys.each do |k|
-  if matches = k.match(/([^\.]+)\.js/)
-    filepart = matches[1]
+assets.keys.each do |asset_type|
+  assets[asset_type].keys.each do |k|
+    if matches = k.match(/([^\.]+)\.#{asset_type}/)
+      filepart = matches[1]
 
-    ['plain','min','min_gz'].each do |upload_type|
+      content_type = "application/javascript"
 
-      content_encoding = {}
-      file_ending = 'js'
-      if(upload_type.eql? 'min_gz')
-        content_encoding = {content_encoding: 'gzip'}
-        file_ending = 'min.gz.js'
+      if asset_type.eql? "css"
+        content_type = "text/css"
       end
 
-      if(upload_type.eql? 'min')
-        file_ending = 'min.js'
-      end
+      ['plain','min','min_gz'].each do |upload_type|
 
-      filename = "#{current_rev}/#{filepart}.#{file_ending}"
-      s3args = {bucket: asset_bucket, key: filename, content_type: 'application/javascript', body: javascript_assets[k][upload_type], cache_control: "max-age=31536000"}
-      s3args.merge!(content_encoding)
-      upload_result = s3client.put_object(s3args)
-      if upload_result.etag.nil?
-        puts "File upload failed: #{filename}"
-        exit 1
-      else
-        puts "Uploaded: #{filename}"
+        content_encoding = {}
+        file_ending = asset_type
+        if(upload_type.eql? 'min_gz')
+          content_encoding = {content_encoding: 'gzip'}
+          file_ending = "min.gz.#{asset_type}"
+        end
+
+        if(upload_type.eql? 'min')
+          file_ending = "min.#{asset_type}"
+        end
+
+        filename = "#{current_rev}/#{filepart}.#{file_ending}"
+        s3args = {bucket: asset_bucket, key: filename, content_type: content_type, body: assets[asset_type][k][upload_type], cache_control: "max-age=31536000"}
+        s3args.merge!(content_encoding)
+        upload_result = s3client.put_object(s3args)
+        if upload_result.etag.nil?
+          puts "File upload failed: #{filename}"
+          exit 1
+        else
+          puts "Uploaded: #{filename}"
+        end
       end
+    else
+      puts "Could not determine bucket key from filename: #{k}"
+      exit 1
     end
-  else
-    puts "Could not determine bucket key from filename: #{k}"
-    exit 1
   end
 end
 
