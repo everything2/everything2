5d35461c4c7b9c065699aeed00931af249603543
Jay Bonci <jay@bonci.net>
2022-06-19 22:16:41 -0400

    Cleans out unused mi2 functions
    Fixes #2887

--- www/js/1874924.js
+++ www/js/1874924.js
@@ -2859,10 +2859,6 @@ function replyTo(s, c) {
        }
 }
 
-function replyToEddie (s) {
-       replyTo(s,1);
-}
-
 function clearReply() {
 	document.message_inbox_form.message.value = "";
 }
@@ -2872,49 +2868,4 @@ function checkAll(){
       if(document.message_inbox_form.elements[i].name.substring(0,9) == "deletemsg")
           {document.message_inbox_form.elements[i].checked=true;}
     }
-}
-
-
-function archiveAll() {
-    for (i=0; i < document.message_inbox_form.elements.length; i++) {
-      if(document.message_inbox_form.elements[i].name.substring(0,7) == "archive")
-          {document.message_inbox_form.elements[i].checked=true;}
-    }
-}
-
-function unarchiveAll() {
-    for (i=0; i < document.message_inbox_form.elements.length; i++) {
-      if(document.message_inbox_form.elements[i].name.substring(0,9) == "unarchive")
-          {document.message_inbox_form.elements[i].checked=true;}
-    }
-}
-
-function clearArchive() {
-    for (i=0; i < document.message_inbox_form.elements.length; i++) {
-      if((document.message_inbox_form.elements[i].name.substring(0,7) == "archive") || (document.message_inbox_form.elements[i].name.substring(0,9) == "unarchive"))
-          {document.message_inbox_form.elements[i].checked=false;}
-    }
-}
-
-function enlargeTextbox(textbox) {
-   if (textbox.morph) {
-      textbox.morph('height: 15em;');
-   }
-   else {
-      textbox.style.height = '15em';
-      textbox.rows = 8;
-   }
-}
-
-function shrinkTextbox(textbox) {
-   // Only shrink box back down if no text has been entered
-   if (textbox.value === "") {
-
-      if (textbox.morph) {
-         textbox.morph('height: 2em;');
-      }
-      else {
-         // Don't do jarring shrinking effect if morphing is not enabled
-      }
-   }
 }
