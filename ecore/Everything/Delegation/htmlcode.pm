cc78af951cdd46fb24ada2e402ff2f0eb636077d
Jay Bonci <jay@bonci.net>
2022-06-25 12:11:06 -0400

    Removes the [googleanalytics] htmlcode
    Fixes #2909

--- ecore/Everything/Delegation/htmlcode.pm
+++ ecore/Everything/Delegation/htmlcode.pm
@@ -4126,31 +4126,6 @@ sub guestuserbanner
 
 }
 
-# Also going into a template
-#
-sub googleanalytics
-{
-  my $DB = shift;
-  my $query = shift;
-  my $NODE = shift;
-  my $USER = shift;
-  my $VARS = shift;
-  my $PAGELOAD = shift;
-  my $APP = shift;
-
-  return qq!
-  <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-1314738-1', 'auto');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-  </script>!;
-}
-
 sub static_javascript
 {
   my $DB = shift;
