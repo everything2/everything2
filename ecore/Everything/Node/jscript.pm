a0e830b1795beaa9db6660110ffc853a3fc880be
Jay Bonci <jay@bonci.net>
2022-06-25 18:06:45 -0400

    Removes the jscript node type
    Fixes #2919

--- ecore/Everything/Node/jscript.pm
+++
@@ -1,9 +0,0 @@
-package Everything::Node::jscript;
-use Moose;
-extends 'Everything::Node';
-
-with 'Everything::Node::helper::s3';
-has 'media_extension' => (isa => 'Str', is => 'ro', default => 'js');
-
-__PACKAGE__->meta->make_immutable;
-1;
