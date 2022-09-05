f7e45243e09c72e1ed221afeb997a03d6b8d6c2a
Jay Bonci <jay@bonci.net>
2022-09-05 19:28:51 -0400

    Removes junk log entry
    Fixes #3031

--- www/api/index.pl
+++ www/api/index.pl
@@ -11,7 +11,6 @@ use vars qw($APIr);
 # Inside of mod_perl this should start initialized
 unless($APIr)
 {
-  Everything::printLog("Creating new APIRouter object");
   $APIr ||= Everything::APIRouter->new();
 }
 
