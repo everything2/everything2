2f50d772d378a22d3a6a71b0c620dab12ce773ca
Jay Bonci <jay@bonci.net>
2022-09-18 22:57:51 -0400

    Makes E2 more cloud native by having the environment configure the DB
    Fixes #3060

--- docker/devbuild.sh
+++ docker/devbuild.sh
@@ -13,5 +13,5 @@ if [ "$1" = "full" ]; then
   docker rm e2devdb
   docker image rm everything2/e2db
   docker build -t everything2/e2db -f docker/e2db/Dockerfile .
-  docker run -d --publish 9306:3306 --env E2_DOCKER=development --name=e2devdb --net=e2-dev-net everything2/e2db
+  docker run -d --publish 9306:3306 --env E2_DOCKER=development --env E2_DBSERV=localhost --name=e2devdb --net=e2-dev-net everything2/e2db
 fi
