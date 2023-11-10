8a45287ce853cbdef8c50d89338244761fd79de6
Jay Bonci <jay@bonci.net>
2023-11-10 13:29:06 -0500

    DRYs up the code and encodes even if no compression
    Fixes #3189

--- ecore/Everything/Application.pm
+++ ecore/Everything/Application.pm
@@ -4480,16 +4480,16 @@ sub optimally_compress_page
 
   my $best_compression = $this->best_compression_type;
 
+  $page = Encode::encode("utf8",$page);
   if($best_compression eq "br")
   {
-    $page = IO::Compress::Brotli::bro(Encode::encode("utf8",$page));
+    $page = IO::Compress::Brotli::bro($page);
   }elsif($best_compression eq "deflate") {
     my $outpage = undef;
-    $page = Encode::encode("utf8",$page);
     $page = IO::Compress::Deflate::deflate(\$page => \$outpage);
     $page = $outpage;
   }elsif($best_compression eq "gzip"){
-    $page = Compress::Zlib::memGzip(Encode::encode("utf8",$page));
+    $page = Compress::Zlib::memGzip($page);
   }
 
   return $page;
