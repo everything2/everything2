529fdec3ede97da359c25ec27187c918824279ae
Jay Bonci <jay@bonci.net>
2023-11-14 00:28:34 -0600

    Nuke some old sandboxes with tester code
    Also cleans up a couple of whitespace errors
    
    Fixes #3230

--- ecore/Everything/Node/linktype.pm
+++ ecore/Everything/Node/linktype.pm
@@ -5,7 +5,7 @@ extends 'Everything::Node';
 sub get_link
 {
   my ($self, $from, $to) = @_;
- 
+
   return unless defined($from) and defined($to);
 
   my $linkdata = $self->DB->sqlSelectHashref("*","links","to_node=".$to->node_id." and from_node=".$from->node_id." and linktype=".$self->node_id." limit 1");
